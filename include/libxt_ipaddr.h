fc72a16378d5bbf83715f394c3fc4ed30dbfb71d
Juan Antonio Osorio <jaosorior@gmail.com>
2011-09-16 16:41:47 -0500

    added ipv6 to the userspace plugin

--- include/libxt_ipaddr.h
+++ include/libxt_ipaddr.h
@@ -2,12 +2,16 @@
 #define _LINUX_NETFILTER_LIBXT_IPADDR_H
 
 static void ipaddr_mt_check(unsigned int);
-static void ipaddr_mt_init(struct xt_entry_match *);
+static void ipaddr_mt4_init(struct xt_entry_match *);
+static void ipaddr_mt6_init(struct xt_entry_match *);
 static void ipaddr_mt4_save(const void *, const struct xt_entry_match *);
+static void ipaddr_mt6_save(const void *, const struct xt_entry_match *);
 static void ipaddr_mt4_print(const void *, const struct xt_entry_match *, int);
+static void ipaddr_mt6_print(const void *, const struct xt_entry_match *, int);
 static int ipaddr_mt4_parse(int, char **, int, unsigned int *, const void *, 
 	struct xt_entry_match **);
-static void ipaddr_mt_check(unsigned int);
+static int ipaddr_mt6_parse(int, char **, int, unsigned int *, const void *, 
+	struct xt_entry_match **);
 static void ipaddr_mt_help(void);
 
 #endif /* _LINUX_NETFILTER_LIBXT_IPADDR_H */
--- userspace-plugin/libxt_ipaddr.c
+++ userspace-plugin/libxt_ipaddr.c
@@ -15,7 +15,7 @@ static const struct option ipaddr_mt_opts[] = {
 	{NULL},
 };
 
-static struct xtables_match ipaddr_mt_reg = {
+static struct xtables_match ipaddr_mt4_reg = {
 	.version = XTABLES_VERSION,
 	.name = "ipaddr",
 	.revision = 0,
@@ -23,7 +23,7 @@ static struct xtables_match ipaddr_mt_reg = {
 	.size = XT_ALIGN(sizeof(struct xt_ipaddr_mtinfo)),
 	.userspacesize = XT_ALIGN(sizeof(struct xt_ipaddr_mtinfo)),
 	.help = ipaddr_mt_help,
-	.init = ipaddr_mt_init,
+	.init = ipaddr_mt4_init,
 	.parse = ipaddr_mt4_parse,
 	.final_check = ipaddr_mt_check,
 	.print = ipaddr_mt4_print,
@@ -31,11 +31,33 @@ static struct xtables_match ipaddr_mt_reg = {
 	.extra_opts = ipaddr_mt_opts,
 };
 
+static struct xtables_match ipaddr_mt6_reg = {
+	.version = XTABLES_VERSION,
+	.name = "ipaddr",
+	.revision = 0,
+	.family = NFPROTO_IPV6,
+	.size = XT_ALIGN(sizeof(struct xt_ipaddr_mtinfo)),
+	.userspacesize = XT_ALIGN(sizeof(struct xt_ipaddr_mtinfo)),
+	.help = ipaddr_mt_help,
+	.init = ipaddr_mt6_init,
+	.parse = ipaddr_mt6_parse,
+	.final_check = ipaddr_mt_check,
+	.print = ipaddr_mt6_print,
+	.save = ipaddr_mt6_save,
+	.extra_opts = ipaddr_mt_opts,
+};
 
-static void ipaddr_mt_init(struct xt_entry_match *match)
+
+static void ipaddr_mt4_init(struct xt_entry_match *match)
 {
 	struct xt_ipaddr_mtinfo *info = (void *)match->data;
-	inet_pton(PF_INET, "192.0.2.137", &info->dst.in);
+	inet_pton(PF_INET, "127.0.0.1", &info->dst.in);
+}
+
+static void ipaddr_mt6_init(struct xt_entry_match *match)
+{
+	struct xt_ipaddr_mtinfo *info = (void *)match->data;
+	inet_pton(PF_INET6, "::1", &info->dst.in);
 }
 
 /*
@@ -58,6 +80,29 @@ static void ipaddr_mt4_save(const void *entry, const struct xt_entry_match *matc
 
 		printf("--ipdst %s ",
 				xtables_ipaddr_to_numeric(&info->dst.in));
+	}
+}
+
+/*
+ * Prints the rule.
+ */
+static void ipaddr_mt6_save(const void *entry, const struct xt_entry_match *match)
+{
+	const struct xt_ipaddr_mtinfo *info = (const void *)match->data;
+
+	if (info ->flags & XT_IPADDR_SRC) {
+		if (info->flags & XT_IPADDR_SRC_INV)
+			printf("! ");
+
+		printf("--ipsrc %s ", xtables_ip6addr_to_numeric(&info->src.in6));
+	}
+
+	if (info->flags & XT_IPADDR_DST) {
+		if (info->flags & XT_IPADDR_DST_INV)
+			printf("! ");
+
+		printf("--ipdst %s ",
+				xtables_ip6addr_to_numeric(&info->dst.in6));
 	}
 }
 
@@ -91,6 +136,35 @@ static void ipaddr_mt4_print(const void *entry,
 	}
 }
 
+
+static void ipaddr_mt6_print(const void *entry,
+		const struct xt_entry_match *match, int numeric)
+{
+	const struct xt_ipaddr_mtinfo *info = (const void *)match->data;
+
+	if (info->flags & XT_IPADDR_SRC) {
+		printf("src IP ");
+
+		if (info->flags & XT_IPADDR_SRC_INV)
+			printf("! ");
+
+		if (numeric)
+			printf("%s ", numeric ?
+					xtables_ip6addr_to_numeric(&info->src.in6) :
+					xtables_ip6addr_to_anyname(&info->src.in6));
+	}
+
+	if (info->flags & XT_IPADDR_DST) {
+		printf("dst IP ");
+
+		if (info->flags & XT_IPADDR_DST_INV)
+			printf("! ");
+
+		printf("%s ", numeric ?
+				xtables_ip6addr_to_numeric(&info->dst.in6):
+				xtables_ip6addr_to_anyname(&info->dst.in6));
+	}
+}
 
 static int ipaddr_mt4_parse(int c, char **argv, int invert,
 		unsigned int *flags, const void *entry,
@@ -147,6 +221,58 @@ static int ipaddr_mt4_parse(int c, char **argv, int invert,
 	return false;
 }
 
+static int ipaddr_mt6_parse(int c, char **argv, int invert,
+		unsigned int *flags, const void *entry,
+		struct xt_entry_match **match)
+{
+	struct xt_ipaddr_mtinfo *info = (void *)(*match)->data;
+	struct in6_addr *addrs;
+
+	switch(c) {
+		case '1': /* --ipsrc */
+			if (*flags & XT_IPADDR_SRC)
+				xtables_error(PARAMETER_PROBLEM, "xt_ipaddr: "
+						"Only use \"--ipsrc\" once!");
+
+			*flags |= XT_IPADDR_SRC;
+			info->flags |= XT_IPADDR_SRC;
+
+			if (invert)
+				info->flags |= XT_IPADDR_SRC_INV;
+
+			addrs = xtables_numeric_to_ip6addr(optarg);
+
+			if(addrs == NULL)
+				xtables_error(PARAMETER_PROBLEM, "xt_ipaddr: " 
+					"Parse error at %s", optarg);
+
+			memcpy(&info->src.in6, addrs, sizeof(*addrs));
+
+			return true;
+		case '2': /* --ipdst */
+			if (*flags & XT_IPADDR_DST)
+				xtables_error(PARAMETER_PROBLEM, "xt_ipaddr: "
+						"Only use \"--ipdst\" once!");
+
+			*flags |= XT_IPADDR_DST;
+			info->flags |= XT_IPADDR_DST;
+
+			if (invert)
+				info->flags |= XT_IPADDR_DST_INV;
+
+			// addrs = xtables_numeric_to_ipaddr(optarg, &info->dst.in);
+			addrs = xtables_numeric_to_ip6addr(optarg);
+
+			if (addrs == NULL)
+				xtables_error(PARAMETER_PROBLEM,
+						"Parse error at %s\n", optarg);
+
+			memcpy(&info->dst.in, addrs, sizeof(*addrs));
+			return true;
+	}
+
+	return false;
+}
 
 static void ipaddr_mt_check(unsigned int flags)
 {
@@ -168,6 +294,7 @@ static void ipaddr_mt_help(void)
 
 void _init(void)
 {
-	xtables_register_match(&ipaddr_mt_reg);
+	xtables_register_match(&ipaddr_mt4_reg);
+	xtables_register_match(&ipaddr_mt6_reg);
 }
 
