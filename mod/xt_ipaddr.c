843c280bef3a77babfd0fdfce5437fe06a3fc885
root <root@nat64t.localdomain>
2011-09-16 15:10:33 -0500

    Added IPv6 support using only one match structure

--- mod/xt_ipaddr.c
+++ mod/xt_ipaddr.c
@@ -1,27 +1,15 @@
 #include <linux/kernel.h>
 #include <linux/ip.h>
+#include <net/ipv6.h>
 #include <linux/netfilter/x_tables.h>
 #include <linux/skbuff.h>
 #include <linux/module.h>
 
 #include "xt_ipaddr.h"
 
-/*
-static struct xt_match ipaddr_mt6_reg __read_mostly = {
-	.name = "ipaddr",
-	.revision = 0,
-	.family = NFPROTO_IPV6,
-	.match = ipaddr_mt6,
-	.matchsize = sizeof(struct xt_ipaddr_mtinfo),
-	.me = THIS_MODULE,
-};
-*/
-
-static bool ipaddr_mt(const struct sk_buff *skb, struct xt_action_param *par)
+static bool ipaddr_mt4(const struct iphdr *iph,
+		const struct xt_ipaddr_mtinfo *info)
 {
-	const struct xt_ipaddr_mtinfo *info = par->matchinfo;
-	const struct iphdr *iph = ip_hdr(skb);
-
 	pr_info("SRC=%pI4 \n", &iph->saddr);
 	pr_info("DST=%pI4 \n", &iph->daddr);
 
@@ -42,12 +30,12 @@ static bool ipaddr_mt(const struct sk_buff *skb, struct xt_action_param *par)
 	return true;
 }
 
-/*
-static bool ipaddr_mt6(const struct sk_buff *skb,
-		const struct xt_match_param *par)
+static bool ipaddr_mt6(const struct ipv6hdr *iph,
+		const struct xt_ipaddr_mtinfo *info)
 {
-	const struct xt_ipaddr_mtinfo *info = par->matchinfo;
-	const struct ipv6hdr *iph = ipv6_hdr(skb);
+	pr_info("SRC=%pI6 \n", &iph->saddr);
+	pr_info("DST=%pI6 \n", &iph->daddr);
+
 	if (info->flags & XT_IPADDR_SRC)
 		if ((ipv6_addr_cmp(&iph->saddr, &info->src.in6) != 0) ^
 				!!(info->flags & XT_IPADDR_SRC_INV))
@@ -58,7 +46,15 @@ static bool ipaddr_mt6(const struct sk_buff *skb,
 			return false;
 	return true;
 }
-*/
+
+static bool ipaddr_mt(const struct sk_buff *skb, struct xt_action_param *par)
+{
+	if (par->family == NFPROTO_IPV4)
+		return ipaddr_mt4(ip_hdr(skb), par->matchinfo);
+	else if (par->family == NFPROTO_IPV6)
+		return ipaddr_mt6(ipv6_hdr(skb), par->matchinfo);
+	return false;
+}
 
 static int ipaddr_mt_check(const struct xt_mtchk_param *par)
 {
@@ -84,10 +80,10 @@ static void ipaddr_mt_destroy(const struct xt_mtdtor_param *par)
 	pr_info("Test for address %08lX removed\n", info->src.ip);
 }
 
-static struct xt_match ipaddr_mt4_reg __read_mostly = {
+static struct xt_match ipaddr_mt_reg __read_mostly = {
 	.name = "ipaddr",
 	.revision = 0,
-	.family = NFPROTO_IPV4,
+	.family = NFPROTO_UNSPEC,
 	.match = ipaddr_mt,
 	.checkentry = ipaddr_mt_check,
 	.destroy = ipaddr_mt_destroy,
@@ -96,19 +92,10 @@ static struct xt_match ipaddr_mt4_reg __read_mostly = {
 };
 
 
-static int __init ipaddr_mt_reg(void)
+static int __init ipaddr_mt_init(void)
 {
 	int ret;
-	ret = xt_register_match(&ipaddr_mt4_reg);
-	if (ret < 0)
-		return ret;
-	/*
-	ret = xt_register_match(&ipaddr_mt6_reg);
-	if (ret < 0) {
-		xt_unregister_match(&ipaddr_mt4_reg);
-		return ret;
-	}
-	*/
+	ret = xt_register_match(&ipaddr_mt_reg);
 
 	pr_info("The Netfilter module has been successfully loaded...\n");
 
@@ -117,15 +104,15 @@ static int __init ipaddr_mt_reg(void)
 
 static void __exit ipaddr_mt_exit(void)
 {
-	xt_unregister_match(&ipaddr_mt4_reg);
-	//xt_unregister_match(&ipaddr_mt6_reg);
+	xt_unregister_match(&ipaddr_mt_reg);
 	pr_info("The Netfilter module has been successfully unloaded...\n");
 }
 
-module_init(ipaddr_mt_reg);
+module_init(ipaddr_mt_init);
 module_exit(ipaddr_mt_exit);
 
 MODULE_AUTHOR("Juan Antonio Osorio <jaosorior@gmail.com>");
 MODULE_DESCRIPTION("Xtables: Match source/destination address");
 MODULE_LICENSE("GPL");
 MODULE_ALIAS("ipt_ipaddr");
+MODULE_ALIAS("ip6t_ipaddr");
